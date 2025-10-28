a3c0aa9419432575c6633eca37bb0754e1ea697d
josielly239962-gif <josielly.239962@fmm.org.br>
2025-10-28 11:35:51 -0400

    adicionando informações

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ const fala = document.getElementById("fala");
 
 function falarLenda() {
   const lendas = [
-    "Era uma vez o boto-cor-de-rosa, que se transformava em humano nas noites de festa...",
-    "Dizem que o Curupira protege a floresta e engana caçadores com seus pés virados!",
-    "A lenda da Vitória-Régia fala de uma índia que virou flor ao tentar tocar a lua...",
-    "O Mapinguari é um guardião gigante da selva, com um só olho e um rugido assustador!"
+    "A lenda do Boto conta que, nas festas, ele se transforma em um belo rapaz e encanta as moças da vila.",
+    "O Curupira protege as florestas com seus pés virados, confundindo quem tenta caçar ou destruir a mata.",
+    "A lenda da Vitória-Régia fala de uma índia que virou flor ao tentar tocar o reflexo da lua nas águas.",
+    "O Mapinguari é o guardião da selva, com um só olho e um rugido que ecoa entre as árvores.",
   ];
 
   const aleatoria = lendas[Math.floor(Math.random() * lendas.length)];
   fala.textContent = aleatoria;
 
-  // Fala em voz alta (sintetizador de voz)
   const falaCurumin = new SpeechSynthesisUtterance(aleatoria);
   falaCurumin.lang = "pt-BR";
+  falaCurumin.rate = 1;
   speechSynthesis.speak(falaCurumin);
 }
